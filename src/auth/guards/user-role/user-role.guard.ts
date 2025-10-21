b8ee5f10aa9531758489526e937c3343813b9e64
Johan Daniel Aguirre Arias <aguirreariasjohandaniel@gmail.com>
2025-10-21 17:46:16 -0500

    Add role enum and improve role-based guard logic
    Introduced a ValidRoles enum for consistent role management. Updated the RoleProtected decorator to use the enum and a constant for metadata key. Enhanced UserRoleGuard to check for user existence and valid roles, throwing appropriate exceptions when necessary.

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,13 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from '../../entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -10,7 +17,16 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const validRoles: string [] = this.reflector.get('roles', context.getHandler())
-    console.log("🚀 ~ :13 ~ UserRoleGuard ~ canActivate ~ validRoles:", validRoles)
+    if(!validRoles || validRoles.length === 0) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as User;
+    if(!user) throw new BadRequestException('User not exist');
+    const hasValidRole= user.roles.some(role => validRoles.includes(role));
+    if(!hasValidRole){
+      throw new UnauthorizedException(`User ${user.fullName} does not have a valid role: [${validRoles}]`)
+    }
+
     return true;
   }
 }
